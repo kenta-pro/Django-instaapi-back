20ddd6adf8ef0b78ed7b583fd2c4172453f6d428
kenta-pro <kenta.yuri1026@gmail.com>
2022-07-14 23:16:25 +0900

    heroku-deploy

--- api_insta/settings.py
+++ api_insta/settings.py
@@ -151,6 +151,6 @@ AUTH_USER_MODEL = 'api.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
